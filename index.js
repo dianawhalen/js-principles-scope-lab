63d03d27b32d4eaff803160d08e7ee4e58f0dcf7
dianawhalen <whalendianaj@gmail.com>
2023-06-14 16:12:28 -0600

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBest) {
+  bestCustomer = newBest;
+}
+
+const leastFavoriteCustomer = 'lucy';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'tom';
+}
